a288e2c65e46f7b603b63fc661ea9a6153f62613
Juan José Rubio Iglesias <jjrubio@talentosoftware.com>
2021-08-08 12:16:23 +0200

    Locale-gen

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
   sh -c "$(curl -fsSL http://192.168.1.5:8080/install.sh)"
 ```
 ## TODO
-- [ ] locale-gen
+- [x] locale-gen
 - [ ] /etc/hosts
 - [ ] Crear usuarios
 - [ ] Zona horaria
--- chrootInstall.sh
+++ chrootInstall.sh
@@ -1,10 +1,12 @@
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+sed -i 's/#es_ES.UTF-8/es_ES.UTF-8/g'
+locale-gen
 echo "LANG=es_ES.UTF-8" > /etc/locale.conf
 echo "KEYMAP=es" > /etc/vconsole.conf
 echo "Arch" > /etc/hostname
 mkinitcpio -p linux
 passwd
-#yes | pacman -S refind neovim 
+#yes | pacman -S refind neovim
 yes | pacman -S neovim networkmanager
 systemctl enable NetworkManager
 bootctl --path=/boot install
