657a0c60d8128aaecbaf421b403d74321ee7c5ab
Juan José Rubio Iglesias <jjrubio@talentosoftware.com>
2021-08-08 12:28:44 +0200

    Passwd loop

--- chrootInstall.sh
+++ chrootInstall.sh
@@ -1,16 +1,27 @@
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+echo "LANG=es_ES.UTF-8" > /etc/locale.conf
 sed -i 's/#es_ES.UTF-8/es_ES.UTF-8/g'
 locale-gen
-echo "LANG=es_ES.UTF-8" > /etc/locale.conf
 echo "KEYMAP=es" > /etc/vconsole.conf
 echo "Arch" > /etc/hostname
+
+# /etc/hosts/
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hots
+echo "127.0.1.1	Arch.localdomain	Arch" >> /etc/hosts
+
 mkinitcpio -p linux
-passwd
+until passwd
+do
+    echo "Las contraseñas no son iguales"
+    sleep 0.5
+done
 #yes | pacman -S refind neovim
 yes | pacman -S neovim networkmanager
 systemctl enable NetworkManager
-bootctl --path=/boot install
 
+# EFI Boot
+bootctl --path=/boot install
 echo "title ArchLinux" > /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 #echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
